2a735883ce98e5e0d1ee043a2f6ebcbaadbd1a0a
o0Pleiades0o <redkarroy@gmail.com>
2024-03-31 05:01:12 +0700

    Create register page

--- ledger_stacks/lib/pages/register_page.dart
+++ ledger_stacks/lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -10,6 +11,31 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.purple.shade500,
+        body: Column(
+          children: [
+            Spacer(),
+            Text(
+              "Register",
+              style: TextStyle(
+                  fontSize: 30.sp,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Container(
+              height: 500.h,
+              width: MediaQuery.of(context).size.width,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16.r),
+                      topRight: Radius.circular(16.r),
+                    )),
+              ),
+            )
+          ],
+        ));
   }
 }
