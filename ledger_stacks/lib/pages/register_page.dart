1974dc94630ecf867eded5d1600f157784e977b3
o0Pleiades0o <redkarroy@gmail.com>
2024-03-31 11:32:18 +0700

    create Register screen V.2

--- ledger_stacks/lib/pages/register_page.dart
+++ ledger_stacks/lib/pages/register_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -10,6 +11,49 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.purple.shade500,
+        body: Column(
+          children: [
+            const Spacer(),
+            //Header for Register
+            Padding(
+              padding: const EdgeInsets.only(bottom: 25),
+              child: Column(
+                children: [
+                  Text(
+                    "Register",
+                    style: TextStyle(
+                        fontSize: 30.sp,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  Text(
+                    "Register before login",
+                    style: TextStyle(fontSize: 15.sp, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            //Box for Textfield
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 450.h,
+              decoration: const ShapeDecoration(
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(45),
+                    topRight: Radius.circular(45),
+                  ),
+                ),
+              ),
+              //inside for Textfield
+              child: Column(
+                children: [],
+              ),
+            ),
+          ],
+        ));
   }
 }
