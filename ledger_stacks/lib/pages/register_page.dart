0bee08fbaeb026f389338f4ba1b0926b226c35cd
o0Pleiades0o <redkarroy@gmail.com>
2024-03-31 11:15:58 +0700

    Revert "Create register page"
    This reverts commit 2a735883ce98e5e0d1ee043a2f6ebcbaadbd1a0a.

--- ledger_stacks/lib/pages/register_page.dart
+++ ledger_stacks/lib/pages/register_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -11,31 +10,6 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.purple.shade500,
-        body: Column(
-          children: [
-            Spacer(),
-            Text(
-              "Register",
-              style: TextStyle(
-                  fontSize: 30.sp,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            Container(
-              height: 500.h,
-              width: MediaQuery.of(context).size.width,
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16.r),
-                      topRight: Radius.circular(16.r),
-                    )),
-              ),
-            )
-          ],
-        ));
+    return const Scaffold();
   }
 }
