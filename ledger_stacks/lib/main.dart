11fe8ce4aeaa1e98090fa53f529c208fbb97d959
o0Pleiades0o <redkarroy@gmail.com>
2024-03-31 11:15:31 +0700

    Uncompleted to connect Firebase

--- ledger_stacks/lib/main.dart
+++ ledger_stacks/lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ledger_stacks/pages/register_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyDSbHMF9B6XwxyyDeEaYKYo88_989K3LNA",
+    appId: "1:471169727193:android:cf6463a78832addfacdcc8",
+    messagingSenderId: "471169727193",
+    projectId: "ledgerstacks-2f9e6",
+  ));
   runApp(const MyApp());
 }
 
