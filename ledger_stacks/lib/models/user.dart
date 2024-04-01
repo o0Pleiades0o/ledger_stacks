af2b7ffe9ef8552a13bd21abdf68d67598659a6e
o0Pleiades0o <redkarroy@gmail.com>
2024-04-01 10:39:00 +0700

    Add model User and firestore

---
+++ ledger_stacks/lib/models/user.dart
@@ -0,0 +1,15 @@
+class user {
+  String username;
+  String email;
+  String password;
+
+  user({required this.username, required this.email, required this.password});
+
+  Map<String, dynamic> toJson(){
+    return {
+      'username': username,
+      'email': email,
+      'password': password,
+    }
+  }
+}
