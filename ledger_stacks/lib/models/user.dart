b45af309dad3b34a7234e207637920f6db13b5a7
o0Pleiades0o <redkarroy@gmail.com>
2024-04-01 11:09:39 +0700

    Update user.dart

--- ledger_stacks/lib/models/user.dart
+++ ledger_stacks/lib/models/user.dart
@@ -1,12 +1,18 @@
 class User {
+  String id;
   String username;
   String email;
   String password;
 
-  User({required this.username, required this.email, required this.password});
+  User(
+      {required this.id,
+      required this.username,
+      required this.email,
+      required this.password});
 
   Map<String, dynamic> toJson() {
     return {
+      'String': id,
       'username': username,
       'email': email,
       'password': password,
