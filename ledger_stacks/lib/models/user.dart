f9d089c2ec6117bf369413d606e652d46099d9d3
o0Pleiades0o <redkarroy@gmail.com>
2024-04-01 10:51:32 +0700

    Add firebase_auth

--- ledger_stacks/lib/models/user.dart
+++ ledger_stacks/lib/models/user.dart
@@ -1,15 +1,15 @@
-class user {
+class User {
   String username;
   String email;
   String password;
 
-  user({required this.username, required this.email, required this.password});
+  User({required this.username, required this.email, required this.password});
 
-  Map<String, dynamic> toJson(){
+  Map<String, dynamic> toJson() {
     return {
       'username': username,
       'email': email,
       'password': password,
-    }
+    };
   }
 }
