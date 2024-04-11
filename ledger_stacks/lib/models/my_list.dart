ce5b433310cd033f2278ceeee42f017938e3c6e8
o0Pleiades0o <redkarroy@gmail.com>
2024-04-11 11:58:52 +0700

    Create my_list.dart

---
+++ ledger_stacks/lib/models/my_list.dart
@@ -0,0 +1,17 @@
+class MyList {
+  late String? listId;
+  late String? listName;
+  late int? listAmount;
+  late String? listTranType;
+  late String? listListType;
+  late String? listFrequency;
+
+  MyList({
+    this.listId,
+    this.listName,
+    this.listAmount,
+    this.listTranType,
+    this.listListType,
+    this.listFrequency,
+  });
+}
