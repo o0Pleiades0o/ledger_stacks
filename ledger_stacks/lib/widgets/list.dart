efc66a6dbb22e1e628bb9c935b8add9c6c5e4ef6
o0Pleiades0o <redkarroy@gmail.com>
2024-04-17 15:13:49 +0700

    Update list.dart

--- ledger_stacks/lib/widgets/list.dart
+++ ledger_stacks/lib/widgets/list.dart
@@ -11,35 +11,41 @@ class Listdata extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => ListView.builder(
-          itemCount: controller.myLists.length,
-          itemBuilder: (BuildContext context, int index) {
-            final myList = controller.myLists[index];
-            return Padding(
-              padding: EdgeInsets.only(bottom: 5.h),
-              child: Container(
-                height: 35.h,
-                width: Get.width,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8.r)),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40.w),
-                  child: Row(
-                    children: [
-                      Text(
-                        myList.name,
+    return controller.myLists.isEmpty
+        ? Center(
+            child: Text(
+            "Not Found list data.",
+            style: TextStyle(color: Colors.black.withAlpha(80)),
+          ))
+        : Obx(() => ListView.builder(
+              itemCount: controller.myLists.length,
+              itemBuilder: (BuildContext context, int index) {
+                final myList = controller.myLists[index];
+                return Padding(
+                  padding: EdgeInsets.only(bottom: 5.h),
+                  child: Container(
+                    height: 35.h,
+                    width: Get.width,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.r)),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40.w),
+                      child: Row(
+                        children: [
+                          Text(
+                            myList.name,
+                          ),
+                          const Spacer(),
+                          Text(
+                            myList.amount.toString(),
+                          )
+                        ],
                       ),
-                      const Spacer(),
-                      Text(
-                        myList.amount.toString(),
-                      )
-                    ],
+                    ),
                   ),
-                ),
-              ),
-            );
-          },
-        ));
+                );
+              },
+            ));
   }
 }
