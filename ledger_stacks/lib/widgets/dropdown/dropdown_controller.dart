7529e13429f2f1e7bd047a7dfa864725736be88c
o0Pleiades0o <redkarroy@gmail.com>
2024-04-18 11:28:46 +0700

    add slidable

--- ledger_stacks/lib/widgets/dropdown/dropdown_controller.dart
+++ ledger_stacks/lib/widgets/dropdown/dropdown_controller.dart
@@ -17,10 +17,10 @@ class DropDownTypeController extends GetxController {
 //DropDownFrequencyController
 class DropDownFrequencyController extends GetxController {
   final List<String> items = ['Mon - Fri', 'Sat - Sun', '1st day', '15th'];
-  late final Rx<String> selectedValue;
+  late final Rx<String?> selectedValue;
 
   DropDownFrequencyController() {
-    selectedValue = Rx<String>(items.first);
+    selectedValue = Rx<String?>(null);
   }
 
   void updateSelectedValue(String value) {
