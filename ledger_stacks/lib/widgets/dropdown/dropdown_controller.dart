9da434bb5deaa2486a3e8e74090de5ac7c79d88c
o0Pleiades0o <redkarroy@gmail.com>
2024-04-19 08:43:55 +0700

    Update dropdown_controller.dart

--- ledger_stacks/lib/widgets/dropdown/dropdown_controller.dart
+++ ledger_stacks/lib/widgets/dropdown/dropdown_controller.dart
@@ -2,7 +2,7 @@ import 'package:get/get.dart';
 
 //DropDownTypeController
 class DropDownTypeController extends GetxController {
-  final List<String> items = ['Daily List', 'Temporary', 'Auto'];
+  final List<String> items = ['Daily', 'Temporary', 'Auto'];
   late final Rx<String> selectedValue;
 
   DropDownTypeController() {
