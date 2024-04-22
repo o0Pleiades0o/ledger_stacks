4c2adcd1096de9183750f83122cca8699b6b25b9
o0Pleiades0o <redkarroy@gmail.com>
2024-04-22 17:16:05 +0700

    Revert "Change key"
    This reverts commit 01f538c8899b44e738806297cd32aca629a92de4.

--- ledger_stacks/lib/widgets/card_ledget.dart
+++ ledger_stacks/lib/widgets/card_ledget.dart
@@ -140,14 +140,13 @@ class CardLedger extends StatelessWidget {
 }
 
 class LedgerList extends StatelessWidget {
-  LedgerList({super.key});
+  LedgerList({Key? key});
 
-  final LedgerListController ledgerListController =
-      Get.put(LedgerListController());
+  final LedgerListController ledgerListController = Get.put(LedgerListController());
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => ledgerListController.ledgerList.isEmpty
+    return Obx(() => ledgerListController.ledgerList.isEmpty 
         ? Center(
             child: Text(
               "Not Found list data.",
@@ -155,7 +154,7 @@ class LedgerList extends StatelessWidget {
             ),
           )
         : SizedBox(
-            height: 100.h,
+            height: 100.h, 
             child: ListView.builder(
               itemCount: ledgerListController.ledgerList.length,
               itemBuilder: (BuildContext context, int index) {
@@ -197,3 +196,4 @@ class LedgerList extends StatelessWidget {
           ));
   }
 }
+
