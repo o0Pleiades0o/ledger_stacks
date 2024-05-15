fc819479853e1782a8118414ccbf72f20845bc18
o0Pleiades0o <redkarroy@gmail.com>
2024-05-15 15:52:42 +0700

    Update percent_chart.dart

--- ledger_stacks/lib/widgets/percent_chart.dart
+++ ledger_stacks/lib/widgets/percent_chart.dart
@@ -30,7 +30,7 @@ class PercentChart extends StatelessWidget {
               child: Column(
                 children: [
                   Text(
-                    '${convertToAmount(homeController.financialRiskPercent.value)}%',
+                    '${convertToPercent(homeController.financialRiskPercent.value)}%',
                     style: const TextStyle(fontSize: 45, fontWeight: FontWeight.w600),
                   ),
                   const Text(
